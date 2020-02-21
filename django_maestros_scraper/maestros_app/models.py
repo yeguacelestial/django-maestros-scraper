86f15177b8903b679b80ee32e120ccd66a48c82e
Yegua Celestial <yeguacelestial@gmail.com>
2020-02-20 20:29:12 -0600

    Modified class Meta to fix verbose_name_plural

--- django_maestros_scraper/maestros_app/models.py
+++ django_maestros_scraper/maestros_app/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Search(models.Model):
     search = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Searches'
