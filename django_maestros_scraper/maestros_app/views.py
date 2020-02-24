805b6869cd69c518c228f22bcf5e747d15bf72d3
Yegua Celestial <yeguacelestial@gmail.com>
2020-02-24 16:22:35 -0600

    Created base URL for scraping

--- django_maestros_scraper/maestros_app/views.py
+++ django_maestros_scraper/maestros_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
+from requests.compat import quote_plus
+from . import models
+
+BASE_LISTA_URL = 'http://www.listademaestros.com/fime/buscar/{}'
 
 # Create your views here.
 def home(request):
@@ -8,6 +12,14 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+
+    search = search.replace(' ', '-')
+    final_url = BASE_LISTA_URL.format(search)
+    
+    # print(search)
+    # print(data)
+    # print(final_url)
     stuff_for_frontend = {
         'search': search,
         }
