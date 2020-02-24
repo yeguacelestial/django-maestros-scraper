61a84a1e3f797d3821ba471151f8f9b6da505c31
Yegua Celestial <yeguacelestial@gmail.com>
2020-02-24 15:42:38 -0600

    Added card images

--- django_maestros_scraper/maestros_app/views.py
+++ django_maestros_scraper/maestros_app/views.py
@@ -8,6 +8,6 @@ def new_search(request):
     search = request.POST.get('search')
     stuff_for_frontend = {
         'search': search,
-
         }
+
     return render(request, 'maestros_app/newsearch.html', stuff_for_frontend)
